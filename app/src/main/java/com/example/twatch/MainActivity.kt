c5b0dea7ebb65168a20796be6ef991c075014f26
trulyeven <trulyeven@gmail.com>
2023-09-11 16:01:43 +0900

    constraintlayout 세팅

---
+++ app/src/main/java/com/example/twatch/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.twatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
